4e3d15152a68b51e14654e50fa804087ff1d702b
Tim <Fallenpyr@aol.com>
2023-03-09 19:49:32 -0500

    Added level diversity
    Added moving obstacles, started implementing "quit game" functionality.

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        QuitGame();
+    }
+
+    void QuitGame()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("quitting game...");
+            Application.Quit();
+        }
+        
+    }
+}
